2614c2b683fb381e2f7c5652dfc14a27a4a01217
Gereon Arz <gereonarz@outlook.com>
2019-11-29 12:57:40 +0100

    Update AccountIngress.cs

--- Amsel.Ingress.Rundown/Ingress/AccountIngress.cs
+++ Amsel.Ingress.Rundown/Ingress/AccountIngress.cs
@@ -25,7 +25,7 @@ namespace Amsel.Ingress.Authentication.Ingress
         [NotNull]
         private static readonly APIAddress GetByConnection = new APIAddress(RundownEndpointResources.ENDPOINT,
             RundownEndpointResources.RUNDOWN_SET,
-            RundownSetControllerResources.ENQUEUE_BY_CONNECTION, true);
+            RundownSetControllerResources.ENQUEUE_BY_CONNECTION);
 
         #endregion
 
