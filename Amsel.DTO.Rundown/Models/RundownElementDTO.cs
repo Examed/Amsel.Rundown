aa89dcf0e1ab09405c55a82f69748721ee98659a
Gereon Arz <gereonarz@outlook.com>
2019-12-03 17:19:42 +0100

    DTO

--- Amsel.DTO.Rundown/Models/RundownElementDTO.cs
+++ Amsel.DTO.Rundown/Models/RundownElementDTO.cs
@@ -1,14 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Amsel.Enums.Rundown.Enums;
 
 namespace Amsel.DTO.Rundown.Models
 {
     public class RundownElementDTO
     {
-        public Dictionary<string, string> Values { get; set; }
-        public ERundownSequence Sequence { get; set; }
+        public virtual Guid Id { get; set; }
+        public RundownFunctionDTO Function { get; set; }
+        public virtual bool CanTriggerRundownSet { get; protected set; }
+        public virtual string Type { get; set; }
         public virtual int Delay { get; set; }
+        public virtual int Duration { get; protected set; }
         public virtual string Description { get; set; }
+        public virtual ERundownSequence Sequence { get; set; }
         public virtual string Title { get; set; }
+        public virtual Dictionary<string, string> Values { get; set; }
+        public virtual RundownSetDTO RundownSet { get; set; }
+        public virtual ERundownStatus Status { get; set; }
+    }
+
+    public class RundownParameterDTO
+    {
+        public virtual string Id { get; set; }
+        public virtual bool IsArgument { get; set; }
+        public virtual string DisplayName { get; set; }
+        public virtual EParameterType Type { get; set; }
+    }
+
+    public class RundownFunctionDTO
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Description { get; set; }
+        public virtual EHandlerType HandlerName { get; set; }
+        public virtual string Icon { get; set; }
+        public virtual RundownFunctionDTO RevertFunction { get; set; }
+        public virtual string Title { get; set; }
+        public virtual Dictionary<string, string> Values { get; set; }
+        public virtual List<RundownParameterDTO> Parameters { get; set; }
     }
 }
--- Amsel.DTO.Rundown/Models/RundownSetDTO.cs
+++ Amsel.DTO.Rundown/Models/RundownSetDTO.cs
@@ -1,12 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Amsel.DTO.Rundown.Models
 {
     public class RundownSetDTO
     {
+        public virtual Guid Id { get; set; }
         public virtual string Description { get; set; }
         public List<RundownElementDTO> Elements { get; set; }
+        public List<RundownWidgetDTO> Widgets { get; set; }
         public virtual string Name { get; set; }
         public virtual float Priority { get; set; }
+        public virtual RundownQueueDTO Queue { get; set; }
+        public virtual ERundownStatus Status { get; set; }
+    }
+
+    public enum ERundownStatus
+    {
+        NONE,
+        QUEUED,
+        ACTIVE,
+        COMPLETED,
+        CANCELED
+    }
+
+    public class RundownQueueDTO
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual bool StopOnNew { get; set; }
+    }
+
+    public class RundownWidgetDTO
+    {
     }
 }
