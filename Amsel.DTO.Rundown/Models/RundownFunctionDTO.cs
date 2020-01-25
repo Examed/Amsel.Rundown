ee3b53873b7bfde091141cb4623bf5c2cb523f83
Gereon <gereonarz@outlook.com>
2020-01-26 00:32:24 +0100

    Update RundownFunctionDTO.cs

--- Amsel.DTO.Rundown/Models/RundownFunctionDTO.cs
+++ Amsel.DTO.Rundown/Models/RundownFunctionDTO.cs
@@ -12,25 +12,25 @@ namespace Amsel.DTO.Rundown.Models
 {
     public class RundownFunctionDTO : ShareTenantEntityDTO
     {
-        public  string Description { get; set; }
+        public string Description { get; set; }
 
-        [Required] public  EHandlerType HandlerName { get; set; }
-
-        public  string Icon { get; set; }
+        [Required] public EHandlerType HandlerName { get; set; }
+        [Required] public RundownSequenceType.EType SequenceType { get; set; }
+        public string Icon { get; set; }
 
         // TODO Revert
-        public  RundownFunctionDTO RevertFunction { get; set; }
-        public  string Name { get; set; }
-        public  bool IsTrigger { get; set; }
+        public RundownFunctionDTO RevertFunction { get; set; }
+        public string Name { get; set; }
+        public bool IsTrigger { get; set; }
 
-        public  List<RundownParameterDTO> Parameters { get; protected set; } = new List<RundownParameterDTO>();
+        public List<RundownParameterDTO> Parameters { get; protected set; } = new List<RundownParameterDTO>();
 
         #region IMultiTenantDTO Members
 
 
         #endregion
 
-        public  void AddParameter([NotNull] RundownParameterDTO parameter)
+        public void AddParameter([NotNull] RundownParameterDTO parameter)
         {
             if (parameter == null) throw new ArgumentNullException(nameof(parameter));
             RundownParameterDTO parameterValue = Parameters?.FirstOrDefault(x => x?.Name != null && x.Name.Equals(parameter.Name, StringComparison.OrdinalIgnoreCase));
