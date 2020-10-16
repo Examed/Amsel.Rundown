2f4afd2d5a3ed6e2f2f9d751eccd8103915c6578
Gereon <gereonarz@outlook.com>
2020-10-16 18:06:21 +0200

    Update DonationData.cs

--- Amsel.Models.Rundown/Models/DonationData.cs
+++ Amsel.Models.Rundown/Models/DonationData.cs
@@ -2,10 +2,13 @@
 using System;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Amsel.Models.Rundown.Models {
-    [ComplexType]
-    public class DonationData : DataEntity {
-        protected DonationData() { }
+namespace Amsel.Models.Rundown.Models
+{
+    public class DonationData : DataEntity
+    {
+        protected DonationData()
+        {
+        }
 
         public DonationData(string user, double amount)
         {
@@ -15,6 +18,7 @@ namespace Amsel.Models.Rundown.Models {
         }
 
         public virtual double Amount { get; set; }
+
         public virtual string User { get; set; }
 
         public override object GetData() => new { Amount, User };
